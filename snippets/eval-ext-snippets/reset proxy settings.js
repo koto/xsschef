0bbcfe85c7a92d0f3829b1007748345c66ea0642
Krzysztof Kotowicz <kkotowicz@gmail.com>
2012-07-24 19:05:14 -0700

    small bugfixes

--- snippets/eval-ext-snippets/reset proxy settings.js
+++ snippets/eval-ext-snippets/reset proxy settings.js
@@ -1,6 +1,10 @@
 // requires proxy permission
 try {
-   chrome.proxy.settings.clear({'incognito': false},__logEval);
+   chrome.proxy.settings.clear({'incognito': false},function() {__logEval('cleared')});
 } catch (e){
-   __logEval(false);
+   try {
+   chrome.proxy.settings.clear({scope: 'regular'},function() {__logEval('cleared with new API')});
+   } catch (e) {
+   __logEval('error');
+   }
 }
